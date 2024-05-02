28d137a1dad6d96b3074e0776f0d460e6fc09648
mhddhimas <kinjunior14@gmail.com>
2024-05-02 17:38:38 +0700

    add config vercel.json

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ db.connect((err) => {
     if (err) {
         console.error('Error connecting to database:', err);
         return;
-    } .
+    }
     console.log('Connected to database');
 });
 
