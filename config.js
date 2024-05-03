9c70015182cada3d6d1a4d3739c023d9381a31fd
mhddhimas <kinjunior14@gmail.com>
2024-05-03 14:04:40 +0700

    baru2

--- config.js
+++ config.js
@@ -4,17 +4,7 @@ const db = mysql.createConnection({
     host: 'sql6.freesqldatabase.com',
     user: 'sql6702732',
     password: '3ZfrDxtYD6',
-    database: 'sql6702732',
-    port: 3306 // Ganti dengan port yang sesuai jika perlu
-});
-
-// Mengaktifkan koneksi
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to database');
+    database: 'sql6702732'
 });
 
 module.exports = db;
